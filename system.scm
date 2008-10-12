55ef94ab16d4dd04ebd956d8eaeb2dfed8539bbb
Peter Danenberg <pcd@wikitex.org>
2008-10-12 13:26:02 -0700

    TODO: moving planets; last plan in raws

--- system.scm
+++ system.scm
@@ -16,53 +16,69 @@
                   (keplerian-panner-fader
                    (pure-tone hertz (planet-tone planet) size volume)
                    planet
-                   earth-moon-barycenter
+                   jupiter
                    days
                    duration))
-                (list mercury
-                      venus
-                      mars
-                      jupiter
-                      saturn
-                      uranus
-                      neptune
-                      pluto))))
-      (let-values (((mercury
+                (list
+                 mercury
+                 venus
+                 earth-moon-barycenter
+                 mars
+;;;                  jupiter
+                 saturn
+                 uranus
+                 neptune
+                 pluto
+                 ))))
+      (let-values (((
+                     mercury
                      venus
+                     earth-moon-barycenter
                      mars
-                     jupiter
+;;;                      jupiter
                      saturn
                      uranus
                      neptune
-                     pluto)
+                     pluto
+                     )
                     (apply values planets)))
         (loop ((for t (up-from 0 (to duration))))
-              (let-values (((mercury-l mercury-r) (mercury t))
+              (let-values (
+                           ((mercury-l mercury-r) (mercury t))
                            ((venus-l venus-r) (venus t))
+                           ((earth-moon-barycenter-l
+                             earth-moon-barycenter-r) (venus t))
                            ((mars-l mars-r) (mars t))
-                           ((jupiter-l jupiter-r) (jupiter t))
+;;;                            ((jupiter-l jupiter-r) (jupiter t))
                            ((saturn-l saturn-r) (saturn t))
                            ((uranus-l uranus-r) (uranus t))
                            ((neptune-l neptune-r) (neptune t))
-                           ((pluto-l pluto-r) (pluto t)))
+                           ((pluto-l pluto-r) (pluto t))
+                           )
                 (if (zero? (modulo t 1000))
                     (debug (- duration t)))
                 (endian-port-write-int1 out (+ ((mixer
                                                  mercury-l
                                                  venus-l
+                                                 earth-moon-barycenter-l
                                                  mars-l
-                                                 jupiter-l
+;;;                                                  jupiter-l
                                                  saturn-l
+                                                 uranus-l
                                                  neptune-l
-                                                 pluto-l) t)
+                                                 pluto-l
+                                                 ) t)
                                                (expt 2 (- size 1))))
                 (endian-port-write-int1 out (+ ((mixer
                                                  mercury-r
                                                  venus-r
+                                                 earth-moon-barycenter-r
                                                  mars-r
-                                                 jupiter-r
+;;;                                                  jupiter-r
                                                  saturn-r
+                                                 uranus-l
                                                  neptune-r
-                                                 pluto-r) t)
+                                                 pluto-r
+                                                 ) t)
                                                (expt 2 (- size 1)))))))))
   (close-endian-port out))
